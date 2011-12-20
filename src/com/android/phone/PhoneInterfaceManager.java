1b7849bc8359d25f06ae23cecd3e81443e208546
thekraven <savethestyle1@gmail.com>
2011-12-20 15:54:54 -0600

    new lockscreen rings rev2
    Change-Id: I228250d07089b563fcee561ec929ad9840feec6d

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -142,10 +142,10 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
                     if (phoneType == Phone.PHONE_TYPE_CDMA) {
                         // CDMA: If the user presses the Power button we treat it as
                         // ending the complete call session
-                        hungUp = PhoneUtils.hangupRingingAndActive(mPhone);
+                        hungUp = false;
                     } else if (phoneType == Phone.PHONE_TYPE_GSM) {
-                        // GSM: End the call as per the Phone state
-                        hungUp = PhoneUtils.hangup(mCM);
+                        // GSM: Do not hangup
+                        hungUp = false;
                     } else {
                         throw new IllegalStateException("Unexpected phone type: " + phoneType);
                     }
